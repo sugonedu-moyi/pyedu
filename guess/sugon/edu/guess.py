f6cf64d7a1c5a3997e438b2a362f367a3c8d95ee
moylaugh <moylaugh@gmail.com>
2018-02-03 19:58:24 +0800

    add comment

--- guess/sugon/edu/guess.py
+++ guess/sugon/edu/guess.py
@@ -2,7 +2,13 @@ import random
 
 
 def is_valid_number(s):
-    """判断字符串s是否表示有效的数字"""
+    """判断字符串s是否表示有效的数字。
+
+    返回Boolean类型"""
+    '*** 在这里补充你的代码 ***'
+    '*** 首先判断字符串s是否表示数字 ***'
+    '*** 如果是数字，转为int类型，并进一步判断是否在1-100之间 ***'
+    '*** 提示：使用字符串的isdigit()方法 ***'
     return s.isdigit() and 1 <= int(s) <= 100
 
 
@@ -14,12 +20,18 @@ def main():
     # 猜数字的次数
     num_guesses = 0
     guess_str = input('请猜一个1-100之间的数字：')
+    '*** 在这里补充你的代码 ***'
+    '*** 修改while条件，当未猜对的时候重复循环 ***'
     while not guess_ok:
         if is_valid_number(guess_str):
             num_guesses += 1
             guess = int(guess_str)
+            '*** 在这里补充你的代码 ***'
+            '*** 修改这个条件 ***'
             if guess < number:
                 guess_str = input('太小了。再猜一次：')
+            # '*** 在这里补充你的代码 ***'
+            # '*** 修改这个条件 ***'
             elif guess > number:
                 guess_str = input('太大了。再猜一次：')
             else:
